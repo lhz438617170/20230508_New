833ae81837eb0c92e54064e16d8d24276f5fe6e6
438617170 <438617170@qq.com>
2023-04-04 16:03:25 +0800

    新版本

---
+++ blender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CeBianLan
+{
+
+    public class blender
+    {
+
+        public static void blender1() 
+        {
+            //SerialPort serialPort1 = new SerialPort();
+            
+            
+        }
+    }
+}
